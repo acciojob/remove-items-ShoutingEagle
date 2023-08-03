3ac4f1ce9023bb1dd0feb7a2a7462b3201f916ea
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-08-03 12:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeSelectedColor(){
+	var selectElement = document.getElementById("colorSelect");
+    var selectedIndex = selectElement.selectedIndex;
+    if(selectedIndex !== -1){
+        selectElement.remove(selectedIndex);
+    }
+}
